f8293f1a47e1dc17f0cbfc81e91326534317149f
Madixxx22 <vysokolyan2016@gmail.com>
2023-03-26 01:06:58 +0300

    deleted duplicate code for tests

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,98 +1,22 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.core.exceptions import ObjectDoesNotExist
-from task_manager.tasks.models import Tasks
+from django.db import models
+from typing import Any
 
-from task_manager.utils import get_test_data
-from task_manager.users.models import CustomUser
+from task_manager.tasks.models import Tasks
+from task_manager.tests import TestBase, TestDataBase
 
 
 # Create your tests here.
-class TestTasks(TestCase):
-    fixtures = ['tasks.json', 'users.json', 'statuses.json']
+class TestTasks(TestDataBase, TestBase):
+    model = Tasks
+    fixtures = ['users.json', 'statuses', 'tasks.json']
+    path_test_data = "test_task.json"
+    show_url = 'tasks:show_tasks'
+    create_url = 'tasks:create_task'
+    edit_url = 'tasks:edit_task'
+    delete_url = 'tasks:delete_task'
 
-    @classmethod
-    def setUpTestData(cls):
-        user_data = get_test_data('test_user.json')
-        exist_user_data = user_data['existing']
-        cls.user = CustomUser.objects.get(username=exist_user_data['username'])
-        cls.test_data = get_test_data('test_task.json')
-
-    def assertTasks(self, task, task_data):
+    def assertItem(self, task: models.query.QuerySet[Any], task_data: dict):
         self.assertEqual(task.name, task_data['name'])
         self.assertEqual(task.description, task_data['description'])
         self.assertEqual(task.status.pk, task_data['status'])
         self.assertEqual(task.executor.pk, task_data['executor']),
-
-    def test_show_tasks(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('tasks:show_tasks'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_tasks(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('tasks:create_task'))
-        self.assertEqual(response.status_code, 200)
-
-        create_success = self.test_data['create_success']
-        response = self.client.post(
-            reverse('tasks:create_task'),
-            create_success,
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-        created_task = Tasks.objects.get(
-            name=create_success['name']
-        )
-        self.assertTasks(created_task, create_success)
-
-    def test_update_views(self):
-        self.client.force_login(self.user)
-        exist_task_data = self.test_data['existing']
-        exist_task = Tasks.objects.get(
-            name=exist_task_data['name']
-        )
-        response = self.client.get(
-            reverse('tasks:edit_task', args=[exist_task.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-
-    def test_update(self):
-        self.client.force_login(self.user)
-        exist_task_data = self.test_data['existing']
-        new_task_data = self.test_data['new']
-        exist_task = Tasks.objects.get(name=exist_task_data['name'])
-        response = self.client.post(
-            reverse('tasks:edit_task', args=[exist_task.pk]),
-            new_task_data,
-            follow=True
-        )
-        self.assertRedirects(response, reverse('tasks:show_tasks'))
-        updated_status = Tasks.objects.get(
-            name=new_task_data['name']
-        )
-        self.assertTasks(updated_status, new_task_data)
-
-    def test_delete_view(self):
-        self.client.force_login(self.user)
-        exist_task_data = self.test_data['existing']
-        task = Tasks.objects.get(name=exist_task_data['name'])
-        response = self.client.get(
-            reverse('tasks:delete_task', args=[task.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-
-    def test_delete(self):
-        self.client.force_login(self.user)
-        exist_task_data = self.test_data['existing']
-        task = Tasks.objects.get(name=exist_task_data['name'])
-        response = self.client.post(
-            reverse('tasks:delete_task', args=[task.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, reverse('tasks:show_tasks'))
-        with self.assertRaises(ObjectDoesNotExist):
-            Tasks.objects.get(name=exist_task_data['name'])
--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,96 +1,25 @@
-from django.test import TestCase
-from django.urls import reverse
-from task_manager.users.models import CustomUser
-from django.core.exceptions import ObjectDoesNotExist
+from typing import Any
+from django.db import models
 
-from task_manager.utils import get_test_data
+from task_manager.tests import TestBase, TestDataBase
+from task_manager.users.models import CustomUser
 
 
 # Create your tests here.
-class TestUser(TestCase):
+class TestUser(TestDataBase, TestBase):
     fixtures = ['users.json']
+    model = CustomUser
+    path_test_data = "test_user.json"
+    show_url = 'users:show_users'
+    create_url = 'users:register'
+    edit_url = 'users:edit_user'
+    delete_url = 'users:delete_user'
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.test_data = get_test_data('test_user.json')
-
-    def assertUser(self, user, user_data):
+    def assertItem(
+        self,
+        user: models.query.QuerySet[Any],
+        user_data: dict
+    ) -> None:
         self.assertEqual(user.username, user_data['username'])
         self.assertEqual(user.first_name, user_data['first_name'])
         self.assertEqual(user.last_name, user_data['last_name'])
-
-    def test_show_user(self):
-        response = self.client.get(reverse('users:show_users'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_user(self):
-        response = self.client.get(reverse('users:register'))
-        self.assertEqual(response.status_code, 200)
-
-        create_success = self.test_data['create_success']
-        response = self.client.post(
-            reverse('users:register'),
-            create_success,
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-        created_user = CustomUser.objects.get(
-            username=create_success['username']
-        )
-        self.assertUser(created_user, create_success)
-
-    def test_update_views(self):
-        user = CustomUser.objects.get(pk=3)
-        self.client.force_login(user)
-        exist_user_data = self.test_data['existing']
-        exist_user = CustomUser.objects.get(
-            username=exist_user_data['username']
-        )
-        response = self.client.get(
-            reverse('users:edit_user', args=[exist_user.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-
-    def test_update(self):
-        exist_user_data = self.test_data['existing']
-        new_user_data = self.test_data['new']
-        user = CustomUser.objects.get(username=exist_user_data['username'])
-        self.client.force_login(user)
-        exist_user = CustomUser.objects.get(
-            username=exist_user_data['username']
-        )
-        response = self.client.post(
-            reverse('users:edit_user', args=[exist_user.pk]),
-            new_user_data,
-            follow=True
-        )
-
-        self.assertRedirects(response, reverse('users:show_users'))
-        updated_user = CustomUser.objects.get(
-            username=new_user_data['username']
-        )
-        self.assertUser(updated_user, new_user_data)
-
-    def test_delete_view(self):
-        exist_user_data = self.test_data['existing']
-        user = CustomUser.objects.get(username=exist_user_data['username'])
-        self.client.force_login(user)
-        response = self.client.get(
-            reverse('users:delete_user', args=[user.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-
-    def test_delete(self):
-        exist_user_data = self.test_data['existing']
-        user = CustomUser.objects.get(username=exist_user_data['username'])
-        self.client.force_login(user)
-        response = self.client.post(
-            reverse('users:delete_user', args=[user.pk]),
-            follow=True
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, reverse('users:show_users'))
-        with self.assertRaises(ObjectDoesNotExist):
-            CustomUser.objects.get(username=exist_user_data['username'])
